9f3a7e0d93dc9661259fb3f6031e58e649aa7dee
parzuko <jivansh17@gmail.com>
2020-08-12 19:09:02 +0530

    Make Slider For Changing Values of Length of Array

--- algo_visualiser/lib/screens/home_screen.dart
+++ algo_visualiser/lib/screens/home_screen.dart
@@ -7,10 +7,37 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<int> _array = [];
+  double _sizeOfArray = 100;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.fromLTRB(200, 0, 0, 20),
+            child: SizedBox(
+              width: 300.0,
+              child: Slider(
+                value: _sizeOfArray,
+                min: 100,
+                max: 500,
+                divisions: 3,
+                inactiveColor: Palette.textColor,
+                activeColor: Palette.theButton,
+                label: "Set Size Of Array To: ${_sizeOfArray.round()}",
+                onChanged: (double value) {
+                  setState(() {
+                    _sizeOfArray = value;
+                  });
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
       bottomNavigationBar: Row(
         crossAxisAlignment: CrossAxisAlignment.end,
         children: <Widget>[
