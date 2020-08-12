0f150a49b1c463f5f60f5e455f3d734fe0c23fc2
parzuko <jivansh17@gmail.com>
2020-08-12 18:36:29 +0530

    Add All Bottom Buttons And Padding

--- algo_visualiser/lib/screens/home_screen.dart
+++ algo_visualiser/lib/screens/home_screen.dart
@@ -12,13 +12,62 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       body: Column(),
       bottomNavigationBar: Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
         children: <Widget>[
-          Expanded(
-            child: FlatButton(
-              child: Text("Hello"),
-              onPressed: () {},
+          Padding(
+            padding: const EdgeInsets.fromLTRB(9, 0, 0, 30),
+            child: Expanded(
+              child: FlatButton(
+                child: Text(
+                  "BUBBLE SORT",
+                  style: const TextStyle(
+                    color: Palette.textColor,
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.2,
+                    fontFamily: 'Segoe UI',
+                  ),
+                ),
+                onPressed: () {},
+              ),
             ),
-          )
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(10, 0, 0, 30),
+            child: Expanded(
+              child: FlatButton(
+                child: Text(
+                  "QUICK SORT",
+                  style: const TextStyle(
+                    color: Palette.textColor,
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.2,
+                    fontFamily: 'Segoe UI',
+                  ),
+                ),
+                onPressed: () {},
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(10, 0, 0, 30),
+            child: Expanded(
+              child: FlatButton(
+                child: Text(
+                  "INSERTION SORT",
+                  style: const TextStyle(
+                    color: Palette.textColor,
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.2,
+                    fontFamily: 'Segoe UI',
+                  ),
+                ),
+                onPressed: () {},
+              ),
+            ),
+          ),
         ],
       ),
     );
