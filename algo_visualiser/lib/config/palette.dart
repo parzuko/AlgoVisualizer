1828d64ff3ef60a5bccfbb0e7663cd82f340c917
parzuko <jivansh17@gmail.com>
2020-08-12 17:18:20 +0530

    Add Basic Pallete Colors for MVP

---
+++ algo_visualiser/lib/config/palette.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  //Standard Colors everywhere
+  static const Color scaffold = Color(0xff001930);
+  static const Color textColor = Color(0xffFEFEFE);
+  static const Color theButton = Color(0xff626EE3);
+  static const Color brightText = Color(0xffF44292);
+  // Colors For Insertion Sort
+  static const Color insertion_1 = Color(0xffEE3A59);
+  static const Color insertion_2 = Color(0xffE27B47);
+  static const Color insertion_3 = Color(0xffEFCA58);
+  static const Color insertion_4 = Color(0xff46B29E);
+  static const Color insertion_5 = Color(0xff334E5C);
+}
