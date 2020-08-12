de0c7dc778306b8b4276825e0d4b3a4620862ac3
parzuko <jivansh17@gmail.com>
2020-08-12 17:58:51 +0530

    Add Bottom Navigation to HomePage

--- algo_visualiser/lib/screens/home_screen.dart
+++ algo_visualiser/lib/screens/home_screen.dart
@@ -1,8 +1,26 @@
+import 'package:algo_visualizer/config/palette.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(),
+      bottomNavigationBar: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              child: Text("Hello"),
+              onPressed: () {},
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
