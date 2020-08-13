08974ef88f126f01c8739f1d0eedddafe872cef5
parzuko <jivansh17@gmail.com>
2020-08-13 05:40:25 +0530

    Fix Text Postioning

--- algo_visualiser/lib/screens/home_screen.dart
+++ algo_visualiser/lib/screens/home_screen.dart
@@ -41,58 +41,55 @@ class _HomeScreenState extends State<HomeScreen> {
       bottomNavigationBar: Row(
         crossAxisAlignment: CrossAxisAlignment.end,
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.fromLTRB(9, 0, 0, 30),
-            child: Expanded(
-              child: FlatButton(
-                child: Text(
-                  "BUBBLE SORT",
-                  style: const TextStyle(
-                    color: Palette.textColor,
-                    fontSize: 15.0,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 1.2,
-                    fontFamily: 'Segoe UI',
-                  ),
+          Expanded(
+            flex: 1,
+            child: FlatButton(
+              padding: EdgeInsets.fromLTRB(0, 0, 0, 30),
+              child: Text(
+                "BUBBLE SORT",
+                style: const TextStyle(
+                  color: Palette.textColor,
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1,
+                  fontFamily: 'Segoe UI',
                 ),
-                onPressed: () {},
               ),
+              onPressed: () {},
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(10, 0, 0, 30),
-            child: Expanded(
-              child: FlatButton(
-                child: Text(
-                  "QUICK SORT",
-                  style: const TextStyle(
-                    color: Palette.textColor,
-                    fontSize: 15.0,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 1.2,
-                    fontFamily: 'Segoe UI',
-                  ),
+          Expanded(
+            flex: 1,
+            child: FlatButton(
+              padding: EdgeInsets.fromLTRB(0, 0, 0, 30),
+              child: Text(
+                "QUICK SORT",
+                style: const TextStyle(
+                  color: Palette.textColor,
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1,
+                  fontFamily: 'Segoe UI',
                 ),
-                onPressed: () {},
               ),
+              onPressed: () {},
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(10, 0, 0, 30),
-            child: Expanded(
-              child: FlatButton(
-                child: Text(
-                  "INSERTION SORT",
-                  style: const TextStyle(
-                    color: Palette.textColor,
-                    fontSize: 15.0,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 1.2,
-                    fontFamily: 'Segoe UI',
-                  ),
+          Expanded(
+            flex: 1,
+            child: FlatButton(
+              padding: EdgeInsets.fromLTRB(0, 0, 0, 30),
+              child: Text(
+                "SELECTION SORT",
+                style: const TextStyle(
+                  color: Palette.textColor,
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1,
+                  fontFamily: 'Segoe UI',
                 ),
-                onPressed: () {},
               ),
+              onPressed: () {},
             ),
           ),
         ],
