0797d49116ef907c1f0d5e22da4e720a11c8b2f0
parzuko <jivansh17@gmail.com>
2020-08-14 05:49:15 +0530

    Change Flutter Channel and Refactor Code

---
+++ algo_visual/android/app/src/main/kotlin/com/example/algo_visual/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.algo_visual
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- algo_visualiser/android/app/src/main/kotlin/com/example/algo_visualiser/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.algo_visualiser
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
